ed8b18100a67f5c47778672b37711fe1e25d0ead
Dale Larsen <dalelarsen22@gmail.com>
2020-05-21 20:30:58 -0600

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,13 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-146995749-1",
+          cookieDomain: "dalelarsen.com"
+        }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
